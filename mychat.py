87405e8901aecd0b2ad25f4b50b5093ad20debc3
Corwin Smith <corwin.smith@uleth.ca>
2017-11-29 15:58:13 -0700

    Working on Hello Thread process

--- mychat.py
+++ mychat.py
@@ -1,6 +1,7 @@
 from Queue import Queue, Empty
-from threading import Thread
+from threading import Thread, Timer
 from time import sleep
+import threading
 import sys, socket, errno
 
 MYPORT = int(sys.argv[1])
@@ -12,6 +13,7 @@ class Receiver(Thread):
     def __init__(self, queue):
         Thread.__init__(self)
         self.queue = queue
+        self.partners = []
 
     def run(self):
         try:
@@ -31,8 +33,41 @@ class Receiver(Thread):
                 data, addr = s.recvfrom(BUFLEN)
             except OSError as err:
                 print "Cannot receive from socket: {}".format(err.strerror)
-            print "Received Message"
+            if data is "HELLO":
+                self.partners.append(addr)
             self.queue.put(data)
+
+
+
+class Hello(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+
+    def hello(self):
+        try:
+            s = scoket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except:
+            print "Cannot open socket"
+            sys.exit(1)
+
+        try:
+            s.bind(('', MYPORT+1))
+        except:
+            print "Cannot bind to port"
+            sys.exit(1)
+
+        try:
+            s.sendto('HELLO', (THEIRIP, THEIRPORT))
+        except:
+            print 'Cannot send: {}'.format(err.strerror)
+            sys.exit(1)
+
+    def run(self):
+        while True:
+            sleep(5.0)
+            self.hello()
+
 
 
 def main():
@@ -40,10 +75,13 @@ def main():
     r = Receiver(queue)
     r.daemon = True
     r.start()
+    h = Hello()
+    h.daemon = True
+    h.start()
 
     print 'p - prints received messages \ns <msg> - sends message \nq-quits\n'
-    cmd = raw_input('& ')
-    while cmd[0] is not 'q':
+    cmd = raw_input('')
+    while cmd[0] != 'q':
         if cmd[0] is 'p':
             try:
                 while True:
@@ -58,23 +96,9 @@ def main():
                 message += cmd[char]
             send_message(message)
 
-        cmd = raw_input('& ')
+        cmd = raw_input('')
 
     print('So long partner')
-
-
-def send_message(message):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except:
-        print "Cannot open socket"
-        sys.exit(1)
-
-    try:
-        s.sendto(message, (THEIRIP, THEIRPORT))
-    except OSError as err:
-        print "Cannot send: {}".format(err.strerror)
-        sys.exit(1)
 
 
 main()
